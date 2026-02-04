21e229cd1ed3e2dc70518c61b90410663d1f14dc
mrifqyabdallah <mrifqyabdallah@gmail.com>
2026-02-04 17:04:54 +0700

    theme personalizations

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,12 +3,12 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <div className="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+            <div className="flex aspect-square size-8 items-center justify-center">
+                <AppLogoIcon className="size-8" />
             </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-tight font-semibold">
-                    Laravel Starter Kit
+            <div className="grid flex-1 text-left text-lg text-shadow-lg/10 dark:text-shadow-white">
+                <span className="truncate leading-tight font-bold">
+                    mrifqyabdallah
                 </span>
             </div>
         </>
