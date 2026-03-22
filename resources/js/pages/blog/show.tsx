bd8ef3ae06a039ad02da1b54dfaa1bef1a9b7ea4
mrifqyabdallah <mrifqyabdallah@gmail.com>
2026-03-22 14:27:01 +0700

    fix: unsupported feature in youtube iframe

--- resources/js/pages/blog/show.tsx
+++ resources/js/pages/blog/show.tsx
@@ -393,7 +393,6 @@ function MarkdownContent({ content }: MarkdownContentProps) {
                             <iframe
                                 src={`https://www.youtube.com/embed/${match[1]}`}
                                 title="YouTube video"
-                                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                                 allowFullScreen
                                 className="absolute inset-0 h-full w-full"
                             />
