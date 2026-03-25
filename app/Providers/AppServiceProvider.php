f4f7ecbdb33953694c56738a01c94645ac51bf25
mrifqyabdallah <mrifqyabdallah@gmail.com>
2026-03-25 14:45:02 +0700

    config schedule event listeners

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,13 @@ namespace App\Providers;
 
 use App\Policies\StatsPolicy;
 use Carbon\CarbonImmutable;
+use Illuminate\Console\Events\ScheduledBackgroundTaskFinished;
+use Illuminate\Console\Events\ScheduledTaskFailed;
+use Illuminate\Console\Events\ScheduledTaskFinished;
+use Illuminate\Console\Events\ScheduledTaskStarting;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
@@ -27,6 +32,7 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->configureDefaults();
         $this->configurePolicies();
+        $this->configureSchedule();
     }
 
     protected function configureDefaults(): void
@@ -51,5 +57,22 @@ class AppServiceProvider extends ServiceProvider
     protected function configurePolicies(): void
     {
         Gate::policy('stats', StatsPolicy::class);
+    }
+
+    protected function configureSchedule(): void
+    {
+        Event::listen(ScheduledTaskStarting::class, function (ScheduledTaskStarting $event): void {
+            logger('Starting: '.$event->task->getSummaryForDisplay());
+        });
+
+        Event::listen([ScheduledTaskFinished::class, ScheduledBackgroundTaskFinished::class], function (ScheduledTaskFinished|ScheduledBackgroundTaskFinished $event): void {
+            logger('Finished: '.$event->task->getSummaryForDisplay());
+        });
+
+        Event::listen(ScheduledTaskFailed::class, function (ScheduledTaskFailed $event): void {
+            logger()->error('Failed: '.$event->task->getSummaryForDisplay(), [
+                'exception' => $event->exception->getMessage(),
+            ]);
+        });
     }
 }
